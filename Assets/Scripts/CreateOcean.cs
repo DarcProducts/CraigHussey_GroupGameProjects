1948100553bf4779d36f34e652e4aeb35198e11c
DarcProducts <darcproducts@gmail.com>
2023-01-22 05:16:22 -0500

    Initial commit

---
+++ Assets/Scripts/CreateOcean.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public sealed class CreateOcean : MonoBehaviour
+{
+    const int WIDTH = 200;
+    const int LENGTH = 200;
+    const float SCALE = 5;
+
+    void Start()
+    {
+        MeshFilter meshFilter = GetComponent<MeshFilter>();
+        Mesh mesh = new();
+        meshFilter.mesh = mesh;
+
+        Vector3[] vertices = new Vector3[WIDTH * LENGTH];
+        Vector2[] uv = new Vector2[vertices.Length];
+        int[] triangles = new int[(WIDTH - 1) * (LENGTH - 1) * 6];
+
+        for (int z = 0; z < LENGTH; z++)
+        {
+            for (int x = 0; x < WIDTH; x++)
+            {
+                int i = z * WIDTH + x;
+                Vector3 vertex = new Vector3(x - WIDTH / 2, 0, z - LENGTH / 2) * SCALE;
+                vertices[i] = vertex;
+                uv[i] = new Vector2((float)x / (WIDTH - 1), (float)z / (LENGTH - 1));
+            }
+        }
+
+        int t = 0;
+        for (int z = 0; z < LENGTH - 1; z++)
+        {
+            for (int x = 0; x < WIDTH - 1; x++)
+            {
+                int i = z * WIDTH + x;
+                triangles[t] = i;
+                triangles[t + 1] = i + WIDTH;
+                triangles[t + 2] = i + 1;
+                triangles[t + 3] = i + 1;
+                triangles[t + 4] = i + WIDTH;
+                triangles[t + 5] = i + WIDTH + 1;
+                t += 6;
+            }
+        }
+
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+    }
+}
---
+++ Assets/Shaders/Ocean.shader
@@ -0,0 +1,58 @@
+Shader "Custom/Ocean" {
+    Properties {
+        _MainTex ("Base Texture", 2D) = "white" {}
+        _Tiling ("Tiling", float) = 1
+        _Speed ("Speed", float) = 1
+        _Wavelength ("Wavelength", float) = 0.1
+        _WaveHeight ("WaveHeight", float) = 0.1
+    }
+
+    SubShader 
+    {
+        Tags {"Queue"="Geometry"}
+        LOD 200
+        Pass{
+        CGPROGRAM
+        #include "UnityCG.cginc"
+        #pragma vertex vert
+        #pragma fragment frag
+        struct appdata 
+        {
+            float4 vertex : POSITION;
+            float2 uv : TEXCOORD0;
+        };
+
+        struct v2f 
+        {
+            float2 uv : TEXCOORD0;
+            float4 vertex : SV_POSITION;
+        };
+
+        sampler2D _MainTex;
+        float _Speed;
+        float _Wavelength;
+        float _WaveHeight;
+        float _Tiling;
+
+        v2f vert (appdata v) 
+        {
+            v2f o;
+            float x = v.uv.x * _Wavelength + _Speed * _Time.y;
+            float y = v.uv.y * _Wavelength + _Speed * _Time.y;
+            float z = v.vertex.z + _WaveHeight * sin(x + y);
+            o.vertex = UnityObjectToClipPos(float3(v.vertex.x, z, v.vertex.z));
+            o.uv = v.uv;
+            return o;
+        }
+
+        fixed4 frag (v2f i) : SV_Target 
+        {
+            fixed4 col = tex2D(_MainTex, i.uv * _Tiling);
+            return col;
+        }
+        ENDCG
+        }
+    }
+}
+
+
